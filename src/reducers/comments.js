84e9dacffef7430ba97ff11bc5082c9af0760fc0
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-07-03 02:08:11 +0300

    update reducers

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,6 +1,7 @@
-function posts(state = [], action) {
+// every reducer run every time there is action and we need to write logic in the reducer to decide it to update the state or not
+function comments(state = [], action) {
    console.log(state, action);
    return state;
 }
 
-export default posts;
+export default comments;
