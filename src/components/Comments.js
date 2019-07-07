698e9cba87303220f09b37a8de6559e61d219465
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-07-07 14:21:48 +0300

    udpate the comments reducer

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 export class Comments extends Component {
-   // constructor(props) {
-   //    super(props);
-   //    this.commentFormRef = React.createRef();
-   //    this.authorRef = React.createRef();
-   //    this.commentRef = React.createRef();
-   // }
+   constructor(props) {
+      super(props);
+      this.commentFormRef = React.createRef();
+      // this.authorRef = React.createRef();
+      // this.commentRef = React.createRef();
+   }
 
    state = {
       author: '',
@@ -20,13 +20,15 @@ export class Comments extends Component {
       });
    }
 
-   renderComments(comment, i) {
+   renderComments = (comment, i) => {
       return (
          <div className="comment" key={i}>
             <p>
                <strong>{comment.user}</strong>
                {comment.text}
-               <button className="remove-comment">&times;</button>
+               <button
+                  className="remove-comment"
+                  onClick={() => this.props.removeComment(this.props.match.params.postId, i)}>&times;</button>
             </p>
          </div>
       );
@@ -39,13 +41,14 @@ export class Comments extends Component {
       // const author = this.authorRef.current.value;
       // const comment = this.commentRef.current.value;
       this.props.addComment(postId, this.state.author, this.state.comment);
+      this.commentFormRef.current.reset();
    }
 
    render() {
       return (
          <div className='comments'>
             {this.props.postComments.map(this.renderComments)}
-            <form /* ref={this.commentFormRef} */ className="comment-form" onSubmit={this.handleSubmit}>
+            <form ref={this.commentFormRef} className="comment-form" onSubmit={this.handleSubmit}>
                <input type="text" name="author" /* ref={this.authorRef} */ placeholder="author" onChange={this.handleInputChange} />
                <input type="text" name="comment" /* ref={this.commentRef} */ placeholder="comment" onChange={this.handleInputChange} />
                <input type="submit" hidden />
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,38 @@
 // every reducer run every time there is action and we need to write logic in the reducer to decide it to update the state or not
+function postComments(state = [], action) {
+   switch (action.type) {
+      case 'ADD_COMMENT':
+         // return the new state with the new comment
+         return [
+            ...state,
+            {
+               user: action.author,
+               text: action.comment
+            }
+         ];
+      case 'REMOVE_COMMENT':
+         console.log('remove comment');
+         // we need to return without the deleted comment
+         return [
+            // from the start to the one we want to remove
+            ...state.slice(0, action.i),
+            // after the deleted one, to the end
+            ...state.slice(action.i + 1),
+         ];
+      default:
+         return state;
+   }
+}
+
 function comments(state = [], action) {
+   if (typeof action.postId !== 'undefined') {
+      return {
+         // take the current state
+         ...state,
+         // overwrite this post with a new one
+         [action.postId]: postComments(state[action.postId], action)
+      }
+   }
    return state;
 }
 
